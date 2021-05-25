339cae6ed0b8acbcde82c0d27ee4ad05443cf83b
angelacuahutle <angela.cuahutle@gmail.com>
2021-05-25 16:18:03 -0500

    Add delete functionality to the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,8 +35,15 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
-  def article_params
-    params.require(:article).permit(:title, :body)
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
